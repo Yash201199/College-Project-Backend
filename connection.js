1a948a72a0c704cac1c5d1b671fc75f50cb78b78
Yash201199 <88087797+Yash201199@users.noreply.github.com>
2023-04-05 23:39:04 +0530

    Add files via upload

---
+++ connection.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+
+//db connection
+const db = 'mongodb://localhost:27017/College-Project'
+mongoose.connect(db ,{useNewUrlParser:true, useUnifiedTopology:true}
+).then(() => {
+    console.log(`connection succesful`);
+
+}).catch((err) => console.log(err));
